e1af8c1b3b132ba7bd86cd302b4645f187b63fa7
saresend <saresend@usc.edu>
2020-08-24 14:36:11 -0700

    now does successfully parse the text

--- src/main.rs
+++ src/main.rs
@@ -13,24 +13,25 @@ use tantivy::query::PhraseQuery;
 use serde::Serialize;
 use warp::reply::Reply;
 
-const DIR_PREFIX: &str = "/media/saresend/81e4a1e1-4190-4fbf-bc60-f287f3d935cc";
+const DIR_PREFIX: &str = "/mnt/sda3";
 
 mod json_parser;
 
 #[tokio::main]
 async fn main() {
     let schema = build_covid_schema();
-    let target_files = vec![PathBuf::from("/media/saresend/81e4a1e1-4190-4fbf-bc60-f287f3d935cc/covid-19-dataset/document_parses/pdf_json/7fefce09c13ca598e5fb20e02c9f19d840bd2a7a.json")];
+    let target_files = vec![PathBuf::from("/mnt/sda3/covid-19-dataset/document_parses/pdf_json/7fefce09c13ca598e5fb20e02c9f19d840bd2a7a.json")];
     let index_reader =
         build_and_commit_corpus(&schema, target_files.into_iter()).expect("Failed to build corpus");
-
+    println!("Finished creating corpus, now starting server");
     run_server(index_reader, schema).await;
 }
 
 fn build_query(schema: &Schema, query: String) -> PhraseQuery {
+    println!("Query: {}", query);
     let field = schema.get_field("text").unwrap();
     let mut token_vec = vec![];
-    for token in query.split(' ') {
+    for token in query.split("%20") {
         let new_term = tantivy::Term::from_field_text(field, token);
         token_vec.push(new_term);
     }
@@ -48,7 +49,7 @@ fn serialize_documents(schema: &Schema, searcher: &Searcher, docs: &Vec<(f32, ta
 
 
 async fn run_server(index_reader: IndexReader, schema: Schema) {
-    let path = warp::path("/query").and(query::raw()).map(move |query | {
+    let path = warp::path("query").and(query::raw()).map(move |query | {
         let searcher = index_reader.searcher();
         let query = build_query(&schema, query);
         let top_docs = searcher.search(&query, &TopDocs::with_limit(20)); 
@@ -57,7 +58,7 @@ async fn run_server(index_reader: IndexReader, schema: Schema) {
                 let serializable_docs = serialize_documents(&schema, &searcher, &top_docs).unwrap();
                 warp::reply::json(&serializable_docs).into_response()
             },
-            Err(top_docs) => { warp::reply::with_status(warp::reply(), warp::http::StatusCode::from_u16(500).unwrap()).into_response() },
+            Err(_) => { warp::reply::with_status(warp::reply(), warp::http::StatusCode::from_u16(500).unwrap()).into_response() },
         }
 
     });
@@ -100,8 +101,8 @@ fn build_and_commit_corpus<K: Iterator<Item = PathBuf>>(
 fn build_covid_schema() -> Schema {
     let mut schema_builder = Schema::builder();
     schema_builder.add_text_field("title", TEXT | STORED);
-    schema_builder.add_text_field("author", TEXT);
+    schema_builder.add_text_field("author", TEXT | STORED);
     schema_builder.add_text_field("paper_id", TEXT | STORED);
-    schema_builder.add_text_field("text", TEXT);
+    schema_builder.add_text_field("text", TEXT | STORED);
     schema_builder.build()
 }
